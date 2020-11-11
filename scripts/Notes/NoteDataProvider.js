3092a6e2488df03cc11de5269e71e9c378ffecdc
Kyle Nevels <67520364+Knevels1@users.noreply.github.com>
2020-11-11 13:47:18 -0600

    Revert "added in a delete button to each note and made the functionality work."

--- scripts/Notes/NoteDataProvider.js
+++ scripts/Notes/NoteDataProvider.js
@@ -31,9 +31,3 @@ export const saveNote = (note) => {
     .then(getNotes)
     .then(dispatchStateChangeEvent)
 }
-export const deleteNote = noteId => {
-    return fetch(`http://localhost:8088/notes/${noteId}`, {
-        method: "DELETE"
-    })
-        .then(getNotes)
-}
